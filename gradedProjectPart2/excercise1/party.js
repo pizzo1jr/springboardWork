7c84dfa050483c87cc0fed773845e4fd768d4e7b
Pizzo, Joseph <JosephPizzo@rocketmortgage.com>
2026-03-11 14:15:16 -0400

    first commit

---
+++ gradedProjectPart2/excercise1/party.js
@@ -0,0 +1,12 @@
+// Question 2 & Question 3: specifcy primative data types for each variable 
+let partyLocation = "GARDEN"; // String, This will stay as a let as for the party location can change.
+const letterShift = 3; // Number, this will be a const, because for at least this invitation, the letters will shift 3.
+let encryptedMessage = ""; // String, This will stay a let as the message itself will change if party location changes
+let decryptedMessage = ""; // String, This will change when we decode the encrypted message 
+let isSafe = false; // Boolean, this could potentially change based on if party is save or not.
+
+// Question 4: 
+const shiftValue = 3; 
+Number.isInteger(shiftValue);
+// this would give true
+
