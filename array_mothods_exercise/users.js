c599a7835f5f7f823ff31cf8704d3f431e75101c
Pizzo, Joseph <JosephPizzo@rocketmortgage.com>
2026-06-08 12:39:11 -0400

    map exercise

---
+++ array_mothods_exercise/users.js
@@ -0,0 +1,14 @@
+const users = [
+	{firstName: "Alice", lastName: "Johnson", points: 120},
+	{firstName: "Bob", lastName: "Smith", points: 99},
+	{firstName: "Charlie", lastName: "Brown", points: 180}
+];
+
+const updateUsers = users.map(user => ({
+	fullName: `${user.firstName} ${user.lastName}`, 
+	membershipStatus: user.points > 100 ? "Premium" : "Standard"
+}));
+
+console.log(
+	updateUsers
+)
